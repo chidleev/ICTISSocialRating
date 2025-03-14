dc40c655fede69a4ff725d1b40e2b5f140040098
Dmitriy Chidleev <70073164+chidleev@users.noreply.github.com>
2025-03-14 23:19:08 +0300

    Delete HahatonApp directory

--- HahatonApp/env.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-source /home/kapitan/Progects/Django/Mediateka.site/bin/activate && PATH=$PATH:/home/kapitan/Progects/Django/Mediateka.site/bin
--- HahatonApp/notes/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-
-class NoteClass(models.Model):
-    models.CharField(max_lenth=100)
-
